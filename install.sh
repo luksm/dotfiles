2cc04ceaa909ee504a90425481c22a4e78a64efa
Lucas Machado <lucas.machado@verint.com>
2020-11-09 18:38:05 -0800

    Improvements to dotfiles

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bash_profile ~/.profile
+ln -s ~/dotfiles/.bash_colors ~/.bash_colors
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
