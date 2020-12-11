f02d50118c491fea42b98910b2b00442e1ee830d
Lucas Machado <lucas.machado@verint.com>
2020-12-11 12:11:47 -0800

    Adding tmux.conf

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ ln -s ~/dotfiles/.bash_profile ~/.profile
 ln -s ~/dotfiles/.bash_colors ~/.bash_colors
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
